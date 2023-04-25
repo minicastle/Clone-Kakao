54651ce91a350f64a889152979e21a3047eaa49d
unknown <jk598200167@gmail.com>
2023-04-25 19:59:00 +0900

    Homepage example data shift

---
+++ src/Data/CallData.js
@@ -0,0 +1,96 @@
+export const CellDataTop = [
+    {
+        scale:1,
+        icon:'bodo',
+        header:'보도자료',
+        date:'2022.12.30',
+        title:'카카오메이커스, ‘제가버치’ 프로젝트 X ‘배상면주가’ 콜라보 제품 출시',
+        tag:['카카오메이커스','제가버치'],
+        image:'https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/news/60499ef4018500001.png?type=thumb&opt=C630x472.fwebp'
+    },
+    {
+        scale:1,
+        icon:'bodo',
+        header:'보도자료',
+        date:'2022.12.30',
+        title:"카카오, '우리동네 단골시장 프로젝트' 수료식 및 우수상인 시상식 진행",
+        tag:['우리동네단골시장','소신상인'],
+        image:'https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/news/560ab850018500001.jpeg?type=thumb&opt=C630x472.fwebp'
+    },
+    {
+        scale:1,
+        icon:'promise',
+        header:'약속과 책임',
+        date:'',
+        title:'카카오는 당신과 함께 더 나은 세상을 만듭니다.',
+        tag:['약속과책임','esg','소셜임팩트'],
+        image:''
+    },
+    {
+        scale:1,
+        icon:'promise',
+        header:'약속과 책임',
+        date:'',
+        title:'ESG보고서, 카카오의 약속과 책임',
+        tag:['2021년생과','ESG보고서'],
+        image:''
+    },
+    {
+        scale:1,
+        icon:'bodo',
+        header:'보도자료',
+        date:'2022.12.29',
+        title:'카카오, ‘1015 피해지원 협의체’와 함께 피해지원 방안 발표',
+        tag:['1015협의체','지원방안발표'],
+        image:'https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/news/5c7c6024018500001.jpg?type=thumb&opt=C630x472.fwebp'
+    },
+    {
+        scale:1,
+        icon:'bodo',
+        header:'보도자료',
+        date:'2022.12.30',
+        title:'카카오엔터프라이즈, AI 국제 학회에서 올 한 해 총 23편 논문 발표',
+        tag:['카카오엔터프라이즈','AI논문'],
+        image:'https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/news/511a81bf018500001.png?type=thumb&opt=C630x472.fwebp'
+    }
+];
+export const graphData = [
+{
+    scale:1,
+    icon:'jooga',
+    header:'주가정보',
+    date:'',
+    title:'53,100',
+    tag:['2022.12.30 15:19PM'],
+    image:''
+}
+];
+export const CellDatalow = [
+{
+    scale:1,
+    icon:'prodo',
+    header:'고객센터',
+    date:'',
+    title:'어떤 서비스를 도와드릴까요?',
+    tag:['카카오','다음','멜론'],
+    image:''
+},
+{
+    scale:1,
+    icon:'bodo',
+    header:'보도자료',
+    date:'2022.12.20',
+    title:'카카오메이커스, ‘새활용’ 프로젝트로 두 번째 기부... 취약계층 어르신 대상 카디건 전달',
+    tag:['새활용프로젝트','카디건기부'],
+    image:'https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/news/2cd13b38018500001.jpg?type=thumb&opt=C630x472.fwebp'
+},
+{
+    scale:1,
+    icon:'shop',
+    header:'카카오 프라이버시',
+    date:'',
+    title:'데이터의 연결과 보호의 균형을 위한 카카오의 노력',
+    tag:['카카오프라이버시'],
+    image:''
+}
+];
